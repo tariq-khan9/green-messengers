e1616c7c5237aec0ceeb561a5e673f3b1894977c
tariq-khan9 <tariqkhan.cs9@gmail.com>
2023-09-11 11:59:50 +0500

    first commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+import Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,8 +12,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header username='tariq'/>
+        <div>
+        {children}
+        </div>
+        </body>
     </html>
+    </ClerkProvider>
   )
 }
---
+++ src/middleware.js
@@ -0,0 +1,9 @@
+import { authMiddleware } from '@clerk/nextjs';
+
+export default authMiddleware({
+  publicRoutes: ['/', '/register','/login'],
+});
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
