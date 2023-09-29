1634289869d12ea333c19d1069bdfd0228e5dd4f
tariq-khan9 <tariqkhan.cs9@gmail.com>
2023-09-29 21:27:57 +0500

    baseurl 2

--- src/lib/constant.js
+++ src/lib/constant.js
@@ -1,4 +1,1 @@
-export const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.BASE_URL
-    : "https://green-messengers.vercel.app";
+export const BASE_URL = "https://green-messengers.vercel.app";
